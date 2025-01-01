bae0eb5c8f8758eae4ef85d99984e8b7df77e21c
中島ももな <mona@s-chan2.local>
2025-01-02 00:29:00 +0900

    kadai_021フォルダの追加

---
+++ kadai_021/event.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click',()=>{
+    setTimeout(()=>{
+    const childList = document.createElement('li');
+
+    childList.textContent = 'ボタンをクリックしました';
+
+    text.appendChild(childList);
+
+    text.textContent = 'ボタンをクリックしました';
+},2000);    
+
+});
